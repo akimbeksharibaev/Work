00f364d36cea03e4a88f4c63b9562b3529aed48f
Akhmet-09 <joldasbaevagumisay35@gmail.com>
2026-07-01 15:26:08 +0500

    settings,channels commands qosdim

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart,Command
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 Token = os.getenv("API")
 dp = Dispatcher()
-bot = Bot(token=TOKEN)
+bot = Bot(token=Token)
 
 @dp.message(CommandStart())
 async def start(message:Message):
     await message.answer(f"Assalawmagaleykum {message.from_user.first_name} \nXosh kelibsiz")
+
+@dp.message(Command())
+async def settings(message:Message):
+    await message.answer(f"Sazlamalar")
+
+@dp.message(Command())
+async def channels(message:Message):
+    await message.answer(f"Kanallar")
 
 async def main():
     print('bot iske qosildi..')
